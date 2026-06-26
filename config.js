eebfb85c9dff39a95a5a3faffb802ecdf8ae3538
55ashraful <ashrafulhaqedulon@gmail.com>
2026-06-26 13:56:30 +0600

    Add Firebase and YouTube API configuration
    Added Firebase configuration and YouTube API key.

---
+++ config.js
@@ -0,0 +1,12 @@
+// এখানে আপনার সব কনফিগারেশন আলাদা রাখা হলো
+const fbConfig = {
+    apiKey: "AIzaSyA8lHi8CmWk1GAR5YzyNUCaI1nDPn2p9aw",
+    authDomain: "my-you-tube-4b216.firebaseapp.com",
+    databaseURL: "https://my-you-tube-4b216-default-rtdb.firebaseio.com",
+    projectId: "my-you-tube-4b216",
+    storageBucket: "my-you-tube-4b216.firebasestorage.app",
+    messagingSenderId: "436495149968",
+    appId: "1:436495149968:web:5096c57a7be9e17e4aa115"
+};
+
+const YT_KEY = "AIzaSyA14llckJ06AG7n5nsYcxuOdlHC21X1PVo";
